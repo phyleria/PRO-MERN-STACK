d09daa4c7057ebe00f499b8f6e7923f48226c6f3
Phyleria <atienophylis032@gmail.com>
2019-05-27 04:16:41 -0400

    This is a new commit for what I originally planned to be amended

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+const contentNode = document.getElementById('contents');
+
+class IssueFilter extends React.Component {
+ render() {
+  return (
+   <div>This is a placeholder for the issue filter</div>
+  );
+ }
+}
+class IssueTable extends React.Component{
+ render(){
+  return(
+   <div>This is a placeholder text for Issue Table</div>
+  );
+ }
+}
+class IssueAdd extends React.Component {
+ render() {
+  return (
+   <div>This is a placeholder for Issue Add entry form</div>
+  );
+ }
+}
+class IssueList extends React.Component {
+ render() {
+  return (
+   <div>
+    <h1>Issue Tracker</h1>
+    <IssueFilter />
+    <hr />
+    <IssueTable />
+    <hr />
+    <IssueAdd />
+   </div>
+  );
+ }
+}
+ReactDOM.render(<IssueList />, contentNode);
